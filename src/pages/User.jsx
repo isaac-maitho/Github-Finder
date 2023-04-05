7796cef697fbb99e8ec65d5f391f6830f321f13b
wyclif-maitho <isaacmaitho14@gmail.com>
2023-04-05 17:32:28 +0300

    actions clean up and axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,16 +2,23 @@ import {FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
 import { useEffect, useContext } from "react"
 import { Link } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
+import RepoList from '../components/repos/ReposList'
 import { useParams } from "react-router-dom"
 import GithubContext from "../context/github/GithubContext"
+import { getUserAndRepos } from '../context/github/GithubActions'
 function User() {
 
-    const {getUser, user, loading} = useContext(GithubContext)
+    const { user, loading, repos, dispatch } = useContext(GithubContext)
     const params = useParams()
 
     useEffect (() => {
-       getUser(params.login)
-    },[])
+       dispatch({type: 'SET_LOADING'})
+       const getUserData = async() =>{
+        const userData = await getUserAndRepos(params.login)
+        dispatch({type:'GET_USER_AND_REPOS', payload: userData})
+       }
+       getUserData()
+    },[dispatch, params.login])
 
     const {
       name,
@@ -146,6 +153,7 @@ function User() {
             <div className="stat-value pr-5 text-3xl md:text-4xl">{public_gists}</div>
           </div>
         </div>
+        <RepoList repos={repos} />
       </div>
     </>
   )
